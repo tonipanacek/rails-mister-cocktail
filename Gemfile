b2f112ea4ecaa02cd8d5895bee26c7bfe7483db7
Toni Panacek <toni.panacek@gmail.com>
2018-08-10 17:56:48 +0200

    if photo is blank

--- Gemfile
+++ Gemfile
@@ -1,4 +1,4 @@
-ruby '2.3.5'
+ruby '2.5.1'
 
 source 'https://rubygems.org'
 
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   resources :cocktails do
     resources :doses, only: [:new, :create]
   end
+
   resources :doses, only: [:destroy]
 
   root to: 'pages#index'
